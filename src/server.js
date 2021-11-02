bdf03bb7c8449422765fc1c51a00c41b866a6be8
Aseel Alasaad <aseelalasaad@gmail.com>
2021-11-02 20:32:59 +0200

    api server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+'use strict';
+const express=require('express');
+const app= express();
+require('dotenv').config();
+
+const PORT=process.env.PORT||3014;
+
+const notfound=require('./error-handlers/404');
+const foodRouter=require('./routes/food');
+
+app.get('/home',(req,res)=>{
+     res.status(200).send('This is The Home 🥳');
+    
+    })
+    
+app.use(express.json());
+app.use(foodRouter);
+app.use('*',notfound); 
+function start() {
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT}`);
+    });
+  }
+  
+  
+  module.exports = {
+    server: app,
+    start: start
+  }
